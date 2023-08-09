d2124a236e2394da7a66c02c63e724b5d8b5c55b
sreey <sreelakshmi12898@gmail.com>
2023-08-09 12:49:30 +0530

    Get user input and display it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './App.module.css'
 import Inputs from "./components/Inputs";
 
 function App() {
+
+  const [height, setHeight] = useState(150);
+
+  const [weight, setWeight] = useState(50)
+  
   return (
     <div className={styles.app}>
       <h1>BMI Calculator</h1>
       <section className={styles.input}>
-        <Inputs />
+        <Inputs height={height} weight={weight} setHeight={setHeight} setWeight={setWeight} />
         <h3>Your BMI is:</h3>
-        <span>25.1</span>
+        <span>{}</span>
       </section>
       <section>
         
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import styles from './Inputs.module.css';
 
-function Inputs(){
+function Inputs(props){
+
+    const getHeight = (event) =>{
+        props.setHeight(event.target.value);
+    }
+
+    const getWeight = (event) => {
+        props.setWeight(event.target.value);
+    }
+
+    
 
     return(
         <div className={styles.inputs} >
             <div className={styles.control} >
-                <label htmlFor="height">Height</label>
-                <input id='height' type="range" step="1" min="140" max="220" />
+                <label htmlFor="height">Height: {props.height}cm</label>
+                <input id='height' type="range" defaultValue={props.height} step="1" min="140" max="220" onChange={getHeight} />
             </div>
             <div className={styles.control} >
-                <label htmlFor="weight">Weight</label>
-                <input id='weight' type="range" step="1" min="40" max="220" />
+                <label htmlFor="weight">Weight: {props.weight}kg</label>
+                <input id='weight' type="range" defaultValue={props.weight} step="1" min="40" max="220" onChange={(getWeight)} />
             </div>
         </div>
     );
