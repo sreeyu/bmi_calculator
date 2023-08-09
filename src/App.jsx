945247883948c4cbe47f2dbc5109fb80b752cb7d
sreey <sreelakshmi12898@gmail.com>
2023-08-09 12:56:37 +0530

    Calculate BMI and display it

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import styles from './App.module.css'
 import Inputs from "./components/Inputs";
 
@@ -6,7 +6,14 @@ function App() {
 
   const [height, setHeight] = useState(150);
 
-  const [weight, setWeight] = useState(50)
+  const [weight, setWeight] = useState(50);
+
+  const [bmi, setBMI] = useState(22.2);
+
+  useMemo(() => {
+    const heightInMeters = height/100;
+    setBMI((weight / (heightInMeters * heightInMeters)).toFixed(1));
+  },[height, weight])
   
   return (
     <div className={styles.app}>
@@ -14,7 +21,7 @@ function App() {
       <section className={styles.input}>
         <Inputs height={height} weight={weight} setHeight={setHeight} setWeight={setWeight} />
         <h3>Your BMI is:</h3>
-        <span>{}</span>
+        <span>{bmi}</span>
       </section>
       <section>
         
